bf6e88239450a829de9f95ccdf2c346554f49c35
sdcro <sdcrouse91@gmail.com>
2020-09-06 19:27:08 -0700

    Limit beach routes (temporarily) to index and show
    This will change, once I add in the routes and controller actions for editing and updating a beach.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :beaches
+      resources :beaches, only: [:index, :show]
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
