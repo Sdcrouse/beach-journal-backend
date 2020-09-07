500ff84376df30b19eb15932641be161cf53c0fb
sdcro <sdcrouse91@gmail.com>
2020-09-06 19:19:47 -0700

    Add index and show actions to BeachesController

--- app/controllers/api/v1/beaches_controller.rb
+++ app/controllers/api/v1/beaches_controller.rb
@@ -1,2 +1,16 @@
 class Api::V1::BeachesController < ApplicationController
+  def index
+    beaches = Api::V1::Beach.all
+    render json: beaches, status: :ok
+  end
+
+  def show
+    beach = Api::V1::Beach.find_by_id(params[:id])
+
+    if beach
+      render json: beach, status: :ok
+    else
+      render plain: "We could not find this beach. Please try again.", status: :not_found
+    end
+  end
 end
