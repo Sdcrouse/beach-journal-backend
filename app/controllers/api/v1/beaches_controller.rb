503f85a6ba30e4ab297f15095fd4e86315d2eaf5
sdcro <sdcrouse91@gmail.com>
2020-09-09 18:19:36 -0700

    Serialize Location JSON, and use it in the BeachesController

--- app/controllers/api/v1/beaches_controller.rb
+++ app/controllers/api/v1/beaches_controller.rb
@@ -1,14 +1,16 @@
 class Api::V1::BeachesController < ApplicationController
   def index
     beaches = Beach.all
-    render json: BeachSerializer.new(beaches), status: :ok
+    options = { include: [:location] }
+    render json: BeachSerializer.new(beaches, options), status: :ok
   end
 
   def show # I may not actually need this action, if I return all of the data in the index action.
     beach = Beach.find_by_id(params[:id])
 
     if beach
-      render json: BeachSerializer.new(beach), status: :ok
+      options = { include: [:location] }
+      render json: BeachSerializer.new(beach, options), status: :ok
     else
       render plain: "We could not find this beach. Please try again.", status: :not_found
     end
--- app/serializers/beach_serializer.rb
+++ app/serializers/beach_serializer.rb
@@ -1,4 +1,5 @@
 class BeachSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :description, :items_to_bring, :popular_activities, :location, :attractions, :journal_entries
+  attributes :name, :description, :items_to_bring, :popular_activities, :attractions, :journal_entries
+  belongs_to :location
 end
