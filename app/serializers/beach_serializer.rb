d0c14290c44e5452fd51373f0a4c9ef6628b0cf1
sdcro <sdcrouse91@gmail.com>
2020-09-11 15:44:16 -0700

    Change the serialization of a beach's location data
    This, along with the next couple of commits, will make the data much easier to use in a Redux store on the frontend.

--- app/serializers/beach_serializer.rb
+++ app/serializers/beach_serializer.rb
@@ -1,7 +1,17 @@
 class BeachSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :description, :items_to_bring, :popular_activities
-  belongs_to :location
+
+  attribute :location do |beach|
+    loc = beach.location
+
+    {
+      city: loc.city,
+      state: loc.state,
+      country: loc.country
+    }
+  end
+
   has_many :attractions
   has_many :journal_entries
 end
