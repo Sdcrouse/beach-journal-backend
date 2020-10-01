a52f5da86757d91a89e80764142afd5e6c78ddee
sdcro <sdcrouse91@gmail.com>
2020-10-01 00:30:03 -0700

    Create, save, and serialize a new JournalEntry

--- app/controllers/api/v1/journal_entries_controller.rb
+++ app/controllers/api/v1/journal_entries_controller.rb
@@ -1,6 +1,15 @@
 class Api::V1::JournalEntriesController < ApplicationController
   def create
-    puts params
-    render json: params
+    journal_entry = JournalEntry.new(journal_entry_params)
+
+    if journal_entry.save
+      render json: JournalEntrySerializer.new(journal_entry), status: :created
+    end
   end
+
+  private
+
+    def journal_entry_params
+      params.require(:journal_entry).permit(:beach_id, :date, :title, :topics, :entry_text)
+    end
 end
