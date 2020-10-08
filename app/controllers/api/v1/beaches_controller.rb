c4012d76c760945058955a17968fdff81ef8f1a1
sdcro <sdcrouse91@gmail.com>
2020-10-07 21:22:01 -0700

    Remove beach show route and corresponding controller action
    This was helpful for development, but it's not used in the application.

--- app/controllers/api/v1/beaches_controller.rb
+++ app/controllers/api/v1/beaches_controller.rb
@@ -4,16 +4,6 @@ class Api::V1::BeachesController < ApplicationController
     render json: BeachSerializer.new(beaches), status: :ok
   end
 
-  def show # I may not actually need this action, if I return all of the data in the index action.
-    beach = Beach.find_by_id(params[:id])
-
-    if beach
-      render json: BeachSerializer.new(beach), status: :ok
-    else
-      render plain: "We could not find this beach. Please try again.", status: :not_found
-    end
-  end
-
   def create
     puts params
     beach = Beach.new(beach_params)
